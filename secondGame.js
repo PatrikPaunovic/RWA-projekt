60e3c001b3054ebe1f97b4bba39ab9db582409fb
Luka8st <luka.bursic5@gmail.com>
2023-01-08 23:34:55 +0100

    izmijenjena funkcija haversine

--- secondGame.js
+++ secondGame.js
@@ -103,15 +103,15 @@ async function startGame() {
 
 function haversine(lat1, lon1, lat2, lon2) {
     const toRadians = (angle) => angle * (Math.PI / 180);
-    const R = 6371e3; // metres
-    const φ1 = toRadians(lat1);
-    const φ2 = toRadians(lat2);
-    const Δφ = toRadians(lat2 - lat1);
-    const Δλ = toRadians(lon2 - lon1);
+    const R = 6371e3; 
+    const latAngle1 = toRadians(lat1);
+    const latAngle2 = toRadians(lat2);
+    const latDiff = toRadians(lat2 - lat1);
+    const lonDiff = toRadians(lon2 - lon1);
   
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
+              Math.cos(latAngle1) * Math.cos(latAngle2) *
+              Math.sin(lonDiff / 2) * Math.sin(lonDiff / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   
     const distance = R * c;
