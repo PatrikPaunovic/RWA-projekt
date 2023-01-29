27e3c77692afd067cb5224180a79cf8c675723b7
matteo <mplisic@riteh.hr>
2023-01-29 20:45:31 +0100

    asd

--- database-access/server/db.js
+++ database-access/server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "123",
+    password: "doggo",
     host: "localhost",
     port: 5432,
-    database: "guessing_game_db"
+    database: "guessing_game"
 });
 
 module.exports = pool;
